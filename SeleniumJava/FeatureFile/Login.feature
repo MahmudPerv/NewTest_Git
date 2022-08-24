cd1fe237fd74bab49d7acbd61921a37ed5eff6c5
MahmudPerv <mahmudperv@gmail.com>
2022-08-23 22:32:59 -0400

    First Commit

---
+++ SeleniumJava/FeatureFile/Login.feature
@@ -0,0 +1,9 @@
+Feature: Login Test For CRM Application 
+
+	Description: As an user can login the Application with valid credential 
+
+Scenario: User Can open Application with valid credential 
+
+	Given User can go to any Browser 
+	
+ 
---
+++ SeleniumJava/src/test/java/seleniumJavaPractice/PracticeSelenium.java
@@ -0,0 +1,23 @@
+package seleniumJavaPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PracticeSelenium {
+	
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		String color = driver.findElement(By.xpath("//input[@placeholder='Username']")).getCssValue("font");
+		System.out.println(color);
+		
+		
+	}
+
+}
